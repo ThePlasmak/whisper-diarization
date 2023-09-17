f2d886710472206804df36d324e2d1f86deec0f2
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2023-09-18 00:18:00 +0800

    Remove help info

--- diarize.py
+++ diarize.py
@@ -85,7 +85,7 @@ parser.add_argument(
     dest="speaker_duration",
     type=float,
     default=None,
-    help="Duration (in seconds) to split long segments of a single speaker talking. (The script will cut a little bit over the duration.)"
+    help="Duration (in seconds) to split long segments of a single speaker talking."
 )
 
 args = parser.parse_args()
